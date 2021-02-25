403bce92a491af094b53674539c0a48637a9b5b1
Matheus Pissaia <pissaia.matheus03@gmail.com>
2021-02-25 00:42:29 -0300

    Managing Global Errors

--- lib/rocketpay_web/controllers/users_controller.ex
+++ lib/rocketpay_web/controllers/users_controller.ex
@@ -3,22 +3,34 @@ defmodule RocketpayWeb.UsersController do
 
   alias Rocketpay.User
 
+  action_fallback RocketpayWeb.FallbackController
+
+  # Antes:
+
+  # def create(conn, params) do
+  #   params
+  #   |> Rocketpay.create_user()
+  #   |> handle_response(conn)
+  # end
+
+  # defp handle_response({:ok, %User{} = user}, conn) do
+  #   conn
+  #   |> put_status(:created)
+  #   |> render("create.json", user: user)
+  # end
+
+  # Depois:
+
   def create(conn, params) do
-    params
-    |> Rocketpay.create_user()
-    |> handle_response(conn)
-  end
-
-  defp handle_response({:ok, %User{} = user}, conn) do
-    conn
-    |> put_status(:created)
-    |> render("create.json", user: user)
-  end
-
-  defp handle_response({:error, result}, conn) do
-    conn
-    |> put_status(:bad_request)
-    |> put_view(RocketpayWeb.ErrorView)
-    |> render("400.json", result: result)
+    # Com o 'with' podemos fazer 'condicionais' (utilização do 'Pattern Matching')
+    # Se a condição dá 'match', então o bloco 'do' é executado.
+    # Se não der 'match', então é retornado um erro para quem chamou essa função.
+    # Assim não precisamos criar várias vezes funções que lidam com erros.
+    # Obs: podem ser colocadas múltiplas senteças / condições dentro do 'with'.
+    with {:ok, %User{} = user} <- Rocketpay.create_user(params) do
+      conn
+      |> put_status(:created)
+      |> render("create.json", user: user)
+    end
   end
 end
